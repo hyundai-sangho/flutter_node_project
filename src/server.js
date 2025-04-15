56e51b901e5b250f7ed7f2901060ccb0b2aae1ca
조상호 <hyundai_sangho@naver.com>
2025-04-15 16:42:06 +0900

    if (process.env.NODE_ENV !== 'production') { 	require('dotenv').config({ path: './config/.env' }); // Ensure the path is correct } 주석

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config({ path: './config/.env' }); // Ensure the path is correct
-}
+// if (process.env.NODE_ENV !== 'production') {
+// 	require('dotenv').config({ path: './config/.env' }); // Ensure the path is correct
+// }
 
 const express = require('express');
 const app = express();
